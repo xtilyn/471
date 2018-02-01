8e0fcd46ad7d823fa2d44fdb0a7b7aef2bde6b02
Esther <Esther@ChungPC>
2018-02-01 01:52:11 -0700

    updated the createChildren method

--- Tree.java
+++ Tree.java
@@ -32,8 +32,39 @@ public class Tree {
 		
 	}
 	
+	/**
+	 * createChildren method creates an array of children nodes which come from a parent node
+	 * @author Esther Chung
+	 * @param parent the parent Node from which the children come
+	 */
 	public void createChildren(Node parent) {
+		// create an array of nodes
+		Node[] childrenArray;
 		
+		// variables needed
+		int parentMachine = parent.getMachine(); // get the parent's machine #
+		char[] availableTasks = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']; // the available tasks
+		char[] takenTasks = parent.getHistory();  // get the history of the tasks that have been taken so far
+		
+		// Take out the tasks that are already taken from the availableTasks array
+		for (int i = 0; i < parentMachine; i++) {
+			for (int j = 0; j < availableTasks.length; j++) {
+				if (takenTasks[i] == availableTasks[j]) {
+					availableTasks[j] = '';
+					break; 
+				}
+			}
+		}
+		
+		// initialize the children nodes; create nodes for only the available tasks
+		for (int i = 0; i < availableTasks.length; i++) {
+			if (availableTasks[i] != '') {
+				childrenArray[i] = new Node(parent, parentMachine + 1, availableTasks[i]);
+			}
+		}
+		
+		// pass the children array to the parent node
+		parent.setChildren(childrenArray);
 	}
 	
 }
