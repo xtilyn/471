d02de52df8c1ca13a32e97ff2cbf3812ac86d742
jmgrageda <35971193+jmgrageda@users.noreply.github.com>
2018-02-02 12:26:59 -0700

    Update Tree.java
    made changes to calcLowerBound

--- Tree.java
+++ Tree.java
@@ -29,51 +29,48 @@ public class Tree {
 		return Node[index];
  	}
 	
-	public int calcLowerBound(Constraint constraint) {
-		
-		Node calcNode = Node; //the node that we are calculating lower bound for 
-		penalty = constraint.getPenalty(); 
-		char tasksTaken = [calcNode.getTask()];
-		char history = calcNode.getHistory() //assuming calcNode machine/task in calcNode history
+	public int calcLowerBound(Constraint constraint, Node node) {
+
+		Node calcNode = node; //the node that we are calculating lower bound for
+		int[][] penalty = constraint.getPenalty(); //gets penalty 2D array from constraint class
+		char[] tasksTaken;
+		tasksTaken.add(calcNode.getTask());
+		char[] history = calcNode.getHistory(); //assuming calcNode machine/task in calcNode history
 		lowerbound = calcNode.getlowerBound();
-		
+
 		//get the sum of the parent nodes machine/task penalty
-		Node tempNode = Node;
+		Node tempNode = node;
 		for (i = calcNode.getMachine(); i>1; i--) {
 			tempNode = tempNode.getParent();
 			tempTask = tempNode.getTask();
-			tasksTaken = tasksTaken + tempTask // task set in parent nodes no longer available
-			lowerbound += penalty[tempNode.getMachine()][tempNode.getTask().convertInt()] //task is char so need to convert to int to get penalty (convert method in Node for task)
-			history[i] = tempTask
+			tasksTaken.add(tempTask); // task set in parent nodes no longer available
+			lowerbound += penalty[tempNode.getMachine()][tempNode.getTask().convertInt()]; //task is char so need to convert to int to get penalty (convert method in Node for task)
+			history[i] = tempTask;
 		}
-		
+
 		// get the sum of the rest of machine/task penalty (lowest penalty possible for each machine)
 		// note: need to figure out how to check for/implement the constraints (too near, forbidden, forced)
-		int tempPenalty = infinity;
+		double tempPenalty = Double.POSITIVE_INFINITY;
 		for (i = calcNode.getMachine() + 1;i<=8; i++) {
 			for (j = 0;j<=8;i++) {
 				if (i,j).forced(){
 					break;
 				}
-				else if j not in tasksTaken && (i,j).hardTooNear() && !(i,j).forbidden(){ //again tasksTaken has char while j is int  
+				else if j not in tasksTaken && (i,j).hardTooNear() && !(i,j).forbidden(){ //again tasksTaken has char while j is int
 					if tempPenalty >= penalty[i][j] + (i,j).softTooNear(){
 						tempPenalty = penalty[i][j]
 					}
 				}
 			lowerbound += penalty[i][j];
-			tasksTaken += j.convertChar();
 			history[i] = j.convertChar();
 			}
-		
+
 		calcNode.setLowerBound(lowerbound);
 		calcNode.setHistory(history);
 		}
-			
+
 	}
-	
-	public int calcLowerBound(Constraint constraint) {
-		
-	}
+
 	
 	/**
 	 * createChildren method creates an array of children nodes which come from a parent node
